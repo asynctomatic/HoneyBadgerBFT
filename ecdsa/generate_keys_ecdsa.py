bf3c10b43fc2139ab11197bbb51a6c6575bb510d
asynctomatic <asynctomatic@gmail.com>
2020-06-27 05:31:25 +0100

    docker and ecdsa fixes

--- ecdsa/generate_keys_ecdsa.py
+++ ecdsa/generate_keys_ecdsa.py
@@ -1,18 +1,22 @@
-from ecdsa_ssl import *
 import argparse
-import cPickle
+import pickle
+
+from coincurve import PrivateKey
+
+
+def generate_key_list(players):
+    return [PrivateKey().secret for _ in range(players)]
+
 
 def main():
+    """ """
     parser = argparse.ArgumentParser()
-    parser.add_argument('players', help='The number of players');
+    parser.add_argument('players', help='The number of players')
     args = parser.parse_args()
     players = int(args.players)
-    keylist = []
-    for i in range(players):
-        key = KEY()
-        key.generate()
-        keylist.append(key.get_secret())
-    print cPickle.dumps(keylist)
+    keylist = generate_key_list(players)
+    print(pickle.dumps(keylist))
+
 
 if __name__ == '__main__':
     main()
